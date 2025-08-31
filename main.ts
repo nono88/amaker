d03690fb8cfd07b4d4b7d0593abbe402904884d3
nono88 <thiery.arnaud@gmail.com>
2025-08-31 11:24:40 +0200

    Mise à jour pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,9 @@
-music.play(music.stringPlayable("- - C5 D C - D - ", 120), music.PlaybackMode.UntilDone)
-led.plot(1, 2)
+basic.showIcon(IconNames.Asleep)
+huskylens.initI2c()
+huskylens.initMode(protocolAlgorithm.ALGORITHM_OBJECT_RECOGNITION)
 basic.forever(function () {
-	
+    huskylens.request()
+    if (huskylens.isAppear_s(HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
+        music.play(music.stringPlayable("E B C5 A B G A F ", 120), music.PlaybackMode.UntilDone)
+    }
 })
