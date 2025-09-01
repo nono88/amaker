6f0acc1f17e54e94ed64773cbe2d82207f804e30
nono88 <thiery.arnaud@gmail.com>
2025-09-01 09:15:09 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,18 @@
+input.onButtonPressed(Button.A, function () {
+    pins.servoWritePin(AnalogPin.P16, 100)
+})
+input.onButtonPressed(Button.B, function () {
+    pins.servoWritePin(AnalogPin.P16, 80)
+})
 basic.showIcon(IconNames.Asleep)
 huskylens.initI2c()
-huskylens.initMode(protocolAlgorithm.ALGORITHM_OBJECT_RECOGNITION)
+huskylens.initMode(protocolAlgorithm.ALGORITHM_OBJECT_TRACKING)
+pins.setAudioPinEnabled(false)
+pins.servoWritePin(AnalogPin.P16, 90)
 basic.forever(function () {
     huskylens.request()
-    if (huskylens.isAppear_s(HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
-        music.play(music.stringPlayable("E B C5 A B G A F ", 120), music.PlaybackMode.UntilDone)
+    if (huskylens.isAppear(1, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
+        pins.servoWritePin(AnalogPin.P0, 74)
+        pins.servoWritePin(AnalogPin.P1, 70)
     }
 })
